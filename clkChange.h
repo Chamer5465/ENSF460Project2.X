886b18cbc8d91c27edc2b2d2f82de61ddf4fd3db
pagadamag <144310370+pagadamag@users.noreply.github.com>
2025-11-27 21:58:21 -0700

    Add header comments
    Added header comments with author and creation date.

--- clkChange.h
+++ clkChange.h
@@ -1,3 +1,10 @@
+/* 
+ * File:   clkChange.h
+ * Author: Chase Mackenzie, Aaron Montesines, Patricia Agdamag
+ *
+ * Created on November 1, 2025, 1:05 PM
+ */
+
 #ifndef CHANGECLK_H
 #define CHANGECLK_H
 
